51589b14df14ae3b06b404f40f8b60dc88ecbaff
ykisii <ishiiyuuki@gmail.com>
2023-03-13 15:36:41 +0900

    Change eval func to macro

--- test.c
+++ test.c
@@ -3,61 +3,47 @@
 #include <string.h>
 #include "fswrap.h"
 
+#define EXPECT_EQ_STR(l,r) \
+    do {  strcmp(l,r) ? printf("* error "#r"\n") : printf("success "#r"\n"); } while (0)
+
+#define EXPECT_EQ_NUM(l,r) \
+    do {  (l == r) ? printf("success "#r"\n") : printf("* error"#r"\n"); } while (0)
+
 int expect_eq_str(char* l, char* r) {
     return !strcmp(l, r);
-}
-
-int expect_eq_int(int l, int r) {
-    return (l == r);
 }
 
 void param_test() {
     FsWrap fswp = {0};
     int ret = 0;
-    ret = expect_eq_int(FSW_PARAM_ERROR, fswrap_open_sync(NULL, "w", &fswp));
-    if (!ret) printf("error %d %d\n",ret, __LINE__);
-
-    ret = expect_eq_int(FSW_PARAM_ERROR, fswrap_open_sync("./test.txt", "w", NULL));
-    if (!ret) printf("error %d %d\n",ret, __LINE__);
-
-    ret = expect_eq_int(FSW_PARAM_ERROR, fswrap_open_sync("./test.txt", NULL, &fswp));
-    if (!ret) printf("error %d %d\n",ret, __LINE__);    
-
-    ret = expect_eq_int(0, fswrap_open_sync("./test.txt", "w", &fswp));
-    if (!ret) printf("error %d %d\n",ret, __LINE__);
-    
-    ret = expect_eq_int(FSW_PARAM_ERROR, fswrap_write_sync(NULL, 0, &fswp));
-    if (!ret) printf("error %d %d\n",ret, __LINE__);
+    EXPECT_EQ_NUM(FSW_PARAM_ERROR, fswrap_open_sync(NULL, "w", &fswp));
+    EXPECT_EQ_NUM(FSW_PARAM_ERROR, fswrap_open_sync("./test.txt", "w", NULL));
+    EXPECT_EQ_NUM(FSW_PARAM_ERROR, fswrap_open_sync("./test.txt", NULL, &fswp));
+    EXPECT_EQ_NUM(0, fswrap_open_sync("./test.txt", "w", &fswp));
+    EXPECT_EQ_NUM(FSW_PARAM_ERROR, fswrap_write_sync(NULL, 0, 0, &fswp));
 
     char buf[] = "aa55";
-    ret = expect_eq_int(FSW_PARAM_ERROR, fswrap_write_sync(buf, sizeof(buf), NULL));
-    if (!ret) printf("error %d %d\n",ret, __LINE__);
+    EXPECT_EQ_NUM(FSW_PARAM_ERROR, fswrap_write_sync(buf, sizeof(char), sizeof(buf), NULL));
     fswp.fp = NULL;
-    ret = expect_eq_int(FSW_PARAM_ERROR, fswrap_write_sync(buf, sizeof(buf), &fswp));
-    if (!ret) printf("error %d %d\n",ret, __LINE__);
+    EXPECT_EQ_NUM(FSW_PARAM_ERROR, fswrap_write_sync(buf, sizeof(char), sizeof(buf), &fswp));
 }
 
 void open_test() {
     FsWrap fswp = {0};
     int ret = 0;
 
-    ret = expect_eq_int(ENOENT, fswrap_open_sync("./open.txt", "r", &fswp));
-    if (!ret) printf("error %d %d\n",ret, __LINE__);
-    ret = expect_eq_int(0, fswrap_open_sync("./a.txt", "w", &fswp));
-    if (!ret) printf("error %d %d\n",ret, __LINE__);
-    ret = expect_eq_str("./a.txt", fswp.path);
-    if (!ret) printf("error %d %d\n",ret, __LINE__);
-}
+    EXPECT_EQ_NUM(ENOENT, fswrap_open_sync("./open.txt", "r", &fswp));
+    EXPECT_EQ_NUM(0, fswrap_open_sync("./a.txt", "w", &fswp));
+    EXPECT_EQ_STR("./a.txt", fswp.path);
+ }
 
 void write_test() {
    FsWrap fswp = {0};
    int ret = 0;
-   ret = expect_eq_int(0, fswrap_open_sync("./write.txt", "w", &fswp));
-   if (!ret) printf("error %d %d\n",ret, __LINE__);
-
+   EXPECT_EQ_NUM(0, fswrap_open_sync("./write.txt", "w", &fswp));
+   
    char buf[] = "aa55";
-   ret = expect_eq_int(0, fswrap_write_sync(buf, sizeof(buf), &fswp));
-   if (!ret) printf("error %d %d\n",ret, __LINE__);
+   EXPECT_EQ_NUM(0, fswrap_write_sync(buf, sizeof(char), sizeof(buf), &fswp));
 }
 
 int main() {
