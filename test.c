d32e8ad075138045f9d90a3c447f5e3341417b73
ykisii <ishiiyuuki@gmail.com>
2023-03-15 18:34:14 +0900

    Update test

--- test.c
+++ test.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <errno.h>
 #include <string.h>
-#include "fswrap.h"
+#include "du.h"
 
 #define EXPECT_EQ_STR(l,r) \
     do {  strcmp(l,r) ? printf("* error "#r"\n") : printf("success "#r"\n"); } while (0)
@@ -13,44 +13,16 @@ int expect_eq_str(char* l, char* r) {
     return !strcmp(l, r);
 }
 
-void param_test() {
-    FsWrap fswp = {0};
-    int ret = 0;
-    EXPECT_EQ_NUM(FSW_PARAM_ERROR, fswrap_open_sync(NULL, "w", &fswp));
-    EXPECT_EQ_NUM(FSW_PARAM_ERROR, fswrap_open_sync("./test.txt", "w", NULL));
-    EXPECT_EQ_NUM(FSW_PARAM_ERROR, fswrap_open_sync("./test.txt", NULL, &fswp));
-    EXPECT_EQ_NUM(0, fswrap_open_sync("./test.txt", "w", &fswp));
-    EXPECT_EQ_NUM(FSW_PARAM_ERROR, fswrap_write_sync(NULL, 0, 0, &fswp));
-
-    char buf[] = "aa55";
-    EXPECT_EQ_NUM(FSW_PARAM_ERROR, fswrap_write_sync(buf, sizeof(char), sizeof(buf), NULL));
-    fswp.fp = NULL;
-    EXPECT_EQ_NUM(FSW_PARAM_ERROR, fswrap_write_sync(buf, sizeof(char), sizeof(buf), &fswp));
-}
-
-void open_test() {
-    FsWrap fswp = {0};
-    int ret = 0;
-
-    EXPECT_EQ_NUM(ENOENT, fswrap_open_sync("./open.txt", "r", &fswp));
-    EXPECT_EQ_NUM(0, fswrap_open_sync("./a.txt", "w", &fswp));
-    EXPECT_EQ_STR("./a.txt", fswp.path);
- }
-
-void write_test() {
-   FsWrap fswp = {0};
-   int ret = 0;
-   EXPECT_EQ_NUM(0, fswrap_open_sync("./write.txt", "w", &fswp));
-   
-   char buf[] = "aa55";
-   EXPECT_EQ_NUM(0, fswrap_write_sync(buf, sizeof(char), sizeof(buf), &fswp));
-}
 
 int main() {
     printf("[test begin]\n");
-    param_test();
-    open_test();
-    write_test();
+    const char *pattern = "*";
+    const char *path = "./";
+    DU* du = du_init();
+    char** list = du->get_current(path, pattern);
+    if (*list) {
+	
+    }
     printf("[test end]\n");
     return 0;
 }
