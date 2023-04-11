b232b01b53445aa44c572f4255fe9fbcb9e539cb
ykisii <ishiiyuuki@gmail.com>
2023-04-11 11:07:59 +0900

    add: test

--- test.c
+++ test.c
@@ -13,10 +13,23 @@ int expect_eq_str(char* l, char* r) {
     return !strcmp(l, r);
 }
 
-static void t_path_is_null(void) {
-  printf("[path is null]\n");
+static void t_path_is_invalid(void) {
+  printf("[%s]\n", __func__);
   const char *pattern = "*";
-  const char *path = "./ho";
+  const char *path = "./jfalsjh";
+  DirUtil* du = du_init();
+    
+  if (!du) return;
+    
+  char** list = du->get(path, pattern);
+  EXPECT_EQ_NUM(NULL, list);
+  du->close();
+}
+
+static void t_path_is_null(void) {
+  printf("[%s]\n", __func__);
+  const char *pattern = "*";
+  const char *path = NULL;
   DirUtil* du = du_init();
     
   if (!du) return;
@@ -27,6 +40,7 @@ static void t_path_is_null(void) {
 }
 
 static void t_print_dir(const char* path) {
+  printf("[%s]\n", __func__);
   const char *pattern = "*";
   DirUtil* du = du_init();
     
@@ -48,6 +62,7 @@ int main() {
     printf("[test begin]\n");
     t_print_dir("./");
     t_path_is_null();
+    t_path_is_invalid();
     printf("[test end]\n");
     return 0;
 }
